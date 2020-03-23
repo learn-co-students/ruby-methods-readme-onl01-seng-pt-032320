ed6f0a79ab60fd4cdf55af0a744983675be49775
Allan Farca <allan.farca@gmail.com>
2020-03-23 06:54:42 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,8 +1,8 @@
-def greeting
-  puts "Hello World" 
+def greeting #greeting
+  puts "Hello World" #method body
 end
 
-greeting
+greeting   #long way to repeate a method, later to use loops
 greeting
 greeting
 greeting
